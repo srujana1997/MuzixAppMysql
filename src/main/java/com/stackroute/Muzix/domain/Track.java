23b6e689314c22a99a5d18bf28079595b54b5b66
srujana1997 <srujanadaruga@gmail.com>
2019-07-29 15:17:16 +0530

    Added test cases

---
+++ src/main/java/com/stackroute/Muzix/domain/Track.java
@@ -0,0 +1,21 @@
+package com.stackroute.Muzix.domain;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@PropertySource("com.stackroute.resources")
+public class Track {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String comment;
+
+}
